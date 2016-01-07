2d4fc95de2da1847c9b14c5f3d0f26ac172caaf3
Mark Wubben <mark@novemberborn.net>
2016-01-07 18:12:39 +0000

    set up handlebars

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import { createServer } from 'https'
 
+import hbs from 'handlebars-inline-precompile'
+
 const pfx = new Buffer(process.env.PFX_BASE64, 'base64')
+const hello = hbs`Hello world`
 
 createServer({ pfx }, (req, res) => {
-  res.end('Hello world')
+  res.end(hello())
 }).listen(8443)
