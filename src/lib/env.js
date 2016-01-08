a080609f1532a5cf7080fe53c5a57969ed98e04b
Mark Wubben <mark@novemberborn.net>
2016-01-08 16:08:16 +0000

    load environment variables from a .env file
    Use dotenv to load variables from a .env file. Add a module which exports the
    variables used in the application, rather than reading from process.env
    directly.

---
+++ src/lib/env.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config({ silent: true })
+
+const {
+  PFX_BASE64
+} = process.env
+
+export {
+  PFX_BASE64
+}
--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ import { createServer } from 'https'
 
 import hbs from 'handlebars-inline-precompile'
 
-const pfx = new Buffer(process.env.PFX_BASE64, 'base64')
+import { PFX_BASE64 } from './lib/env'
+
 const hello = hbs`Hello world`
 
-createServer({ pfx }, (req, res) => {
+createServer({ pfx: new Buffer(PFX_BASE64, 'base64') }, (req, res) => {
   res.end(hello())
 }).listen(8443)
