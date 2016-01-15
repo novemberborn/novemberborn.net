ad33b04cb5776c302e6afe71c8339da0e93deece
Mark Wubben <mark@novemberborn.net>
2016-01-15 17:56:04 +0000

    only respond to GET or HEAD requests

--- src/server.js
+++ src/server.js
@@ -4,5 +4,11 @@ import { PFX_BASE64 } from './lib/env'
 import { skeleton } from 'glob:templates/*.js'
 
 createServer({ pfx: new Buffer(PFX_BASE64, 'base64') }, (req, res) => {
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    res.writeHead(405)
+    res.end()
+    return
+  }
+
   res.end(skeleton())
 }).listen(8443)
