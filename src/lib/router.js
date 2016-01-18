1b51786628c343fad46b04c1c18159c8aa7ee968
Mark Wubben <mark@novemberborn.net>
2016-01-18 12:05:09 +0000

    abstract request handling, serve static files using router

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,4 +1,10 @@
-import { home, notFound } from 'glob:../templates/*.js'
+import staticFiles from '../static'
+
+import {
+  home,
+  notFound,
+  skeleton
+} from 'glob:../templates/*.js'
 
 const table = {
   '/' () {
@@ -7,9 +13,29 @@ const table = {
 }
 
 export function route (pathname) {
-  if (table.hasOwnProperty(pathname)) {
-    return [200, table[pathname]()]
+  if (staticFiles.has(pathname)) {
+    const { contentType, chunk } = staticFiles.get(pathname)
+    return [200, {
+      'content-type': contentType,
+      'content-length': chunk.length
+    }, chunk]
   }
 
-  return [404, { contentPartial: notFound }]
+  let statusCode = 200
+  let headers = {
+    'content-type': 'text/html; charset=utf-8'
+  }
+
+  let context
+  if (table.hasOwnProperty(pathname)) {
+    context = table[pathname]()
+  } else {
+    statusCode = 404
+    context = { contentPartial: notFound }
+  }
+
+  const body = new Buffer(skeleton(context), 'utf8')
+  headers['content-length'] = body.length
+
+  return [statusCode, headers, body]
 }
--- src/server.js
+++ src/server.js
@@ -3,52 +3,48 @@ import { parse as parseUrl } from 'url'
 
 import { PFX_BASE64 } from './lib/env'
 import { route } from './lib/router'
-import staticFiles from './static'
 
 import { skeleton, serverError } from 'glob:templates/*.js'
 
+const errorBody = new Buffer(skeleton({
+  title: '500 Internal Server Error',
+  contentPartial: serverError
+}), 'utf8')
+
 createServer({ pfx: new Buffer(PFX_BASE64, 'base64') }, (req, res) => {
-  if (req.method !== 'GET' && req.method !== 'HEAD') {
-    res.writeHead(405)
-    res.end()
-    return
-  }
-
-  const { pathname } = parseUrl(req.url)
-
-  if (staticFiles.has(pathname)) {
-    const { contentType, chunk } = staticFiles.get(pathname)
-    res.writeHead(200, {
-      'content-type': contentType,
-      'content-length': chunk.length
-    })
-    res.end(chunk)
-    return
-  }
-
-  let statusCode = 200
-  let body, context
   try {
-    [statusCode, context] = route(pathname)
-    body = skeleton(context)
+    const [statusCode, headers, body] = handleRequest(req)
+    res.writeHead(statusCode, headers)
+    if (req.method !== 'HEAD') {
+      res.write(body)
+    }
+    res.end()
   } catch (err) {
     // TODO: Hook up Bunyan
     console.error(err)
 
-    statusCode = 500
-    body = skeleton({
-      title: '500 Internal Server Error',
-      contentPartial: serverError
-    })
-  } finally {
-    const chunk = new Buffer(body, 'utf8')
-    res.writeHead(statusCode, {
-      'content-type': 'text/html; charset=utf-8',
-      'content-length': chunk.length
-    })
-    if (req.method === 'GET') {
-      res.write(chunk)
+    if (!res.headersSent) {
+      res.writeHead(500)
+      res.end()
     }
-    res.end()
   }
 }).listen(8443)
+
+function handleRequest (req) {
+  if (req.method !== 'GET' && req.method !== 'HEAD') {
+    return [405]
+  }
+
+  try {
+    const { pathname } = parseUrl(req.url)
+    return route(pathname)
+  } catch (err) {
+    // TODO: Hook up Bunyan
+    console.error(err)
+
+    return [500, {
+      'content-type': 'text/html; charset=utf-8',
+      'content-length': errorBody.length
+    }, errorBody]
+  }
+}
