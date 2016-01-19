9790c0f30de841d970dc9490a4022a85a8b8c9bb
Mark Wubben <mark@novemberborn.net>
2016-01-19 12:48:43 +0000

    generate routes for non-favicon static files

--- src/lib/static-files.js
+++ src/lib/static-files.js
@@ -3,6 +3,12 @@ import { readFile } from 'fs'
 import files from 'files:../../static/*.*'
 
 const paths = new Map().set('/favicon.ico', files['favicon.ico'])
+for (const name in files) {
+  if (name === 'favicon.ico') continue
+
+  const file = files[name]
+  paths.set(`/static/${file.tag}/${name}`, file)
+}
 
 const cacheSymbol = Symbol
 function getContents (file) {
