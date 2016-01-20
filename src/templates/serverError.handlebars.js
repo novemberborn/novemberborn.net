ac04096fe303469922eb77e9cdfb77a9d538394a
Mark Wubben <mark@novemberborn.net>
2016-01-20 12:37:25 +0000

    flesh out error pages

--- src/templates/serverError.handlebars.js
+++ src/templates/serverError.handlebars.js
@@ -1,4 +1,6 @@
 import hbs from 'handlebars-inline-precompile'
 
-export default hbs`Something went wrong :(
+export default hbs`<h1>Fife Zero Zero</h1>
+<p><img src="/static/8282a4b8c1df1d7948de10fef3a7dd39/empty-rowboat.jpg" alt="An empty rowboat, signifying the failure in serving the page" class="invert"></p>
+<p>The page you requested could not be found. It may have <a href="https://web.archive.org/web/2012*/https://novemberborn.net{{pathname}}">existed previously</a> though.</p>
 `
