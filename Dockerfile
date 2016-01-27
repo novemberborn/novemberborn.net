bc9ce574253f03a0e8e71454094ee4e0bd7560f5
Mark Wubben <mark@novemberborn.net>
2016-01-27 16:24:01 +0000

    optimize layers in final docker image
    Add node_modules to their own layer, to reduce churn when app code or content changes.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM node:5.5.0-slim
 
 WORKDIR /app
-ADD app.tgz /
-
 ENV NODE_ENV=production
 CMD ["node", "server.js"]
+
+ADD node_modules.tgz ./
+ADD app.tgz ./
--- docker.sh
+++ docker.sh
@@ -8,7 +8,11 @@ docker build -t novemberborn-dot-net-build -f build.Dockerfile .
 tmp=$(mktemp -d)
 
 docker create --name novemberborn-dot-net-build novemberborn-dot-net-build /bin/true
-docker cp novemberborn-dot-net-build:/app - > ${tmp}/app.tgz
+docker cp novemberborn-dot-net-build:/app/. - > ${tmp}/combined.tgz
+
+tar -c --include './node_modules' -f ${tmp}/node_modules.tgz @${tmp}/combined.tgz
+tar -c --exclude './node_modules' -f ${tmp}/app.tgz @${tmp}/combined.tgz
+rm ${tmp}/combined.tgz
 
 cp Dockerfile ${tmp}
 docker build -t novemberborn-dot-net -f ${tmp}/Dockerfile ${tmp}
