294d6f97d5059c8f8f6bf9335bb187bf873687f0
Mark Wubben <mark@novemberborn.net>
2016-07-31 15:28:27 +0100

    Env-based certification switching for npm run docker

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
+
+CERT_NAME=${CERT_NAME:-development}
+
 docker run \
   --rm \
   --name=site \
   -p 443:8443 \
   -p 8443:8443 \
   -e ANY_CLIENT=true \
-  -e CERT_NAME=development \
+  -e CERT_NAME=${CERT_NAME} \
   --env-file=.env \
   novemberborn-dot-net | bunyan
