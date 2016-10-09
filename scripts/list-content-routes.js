c52f99c27b76e7892c21511c91e2253b375c9cf2
Mark Wubben <mark@novemberborn.net>
2016-10-09 17:54:05 +0100

    Fix list-content-routes

--- scripts/list-content-routes.js
+++ scripts/list-content-routes.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict'
 
-const router = require('../lib/router')
+const router = require('../dist/lib/router')
 
 for (const pathname of Object.keys(router.table)) {
   console.log(`https://novemberborn.net${pathname}`)
